115abf4c452cf8003e6c0a6ba1b6f7adc3b11ed4
Mark Powney <mpowney+github@icloud.com>
2020-04-25 22:37:38 +1000

    update

--- v1/Redirect.cs
+++ v1/Redirect.cs
@@ -74,21 +74,21 @@ namespace api.v1
             }
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            RedirectEntity entity = JsonConvert.DeserializeObject<RedirectEntity>(requestBody);
 
-            if (data?.rowKey == null || data?.redirectTo == null) {
+            if (entity.RowKey == null || entity.RedirectTo == null) {
                 return new BadRequestObjectResult($"Please specify the key and redirectTo parameters in the request body");
             }
 
-            log.LogInformation($"Getting Redirect row for values ${claimsPrincipal.Identity.Name} and ${data.rowKey}");
-            RedirectEntity existingEntity = await RedirectEntity.get(redirectTable, claimsPrincipal.Identity.Name, data.rowKey);
+            log.LogInformation($"Getting Redirect row for values {claimsPrincipal.Identity.Name} and {entity.RowKey}");
+            RedirectEntity existingEntity = await RedirectEntity.get(redirectTable, claimsPrincipal.Identity.Name, entity.RowKey);
             if (existingEntity != null) {
-                return new BadRequestObjectResult($"Redirect with ${data.rowKey} already exists for ${claimsPrincipal.Identity.Name}");
+                return new BadRequestObjectResult($"Redirect with {entity.RowKey} already exists for {claimsPrincipal.Identity.Name}");
             }
 
-            bool success = await RedirectEntity.put(redirectTable, claimsPrincipal.Identity.Name, data.rowKey, data.redirectTo, 0, new Dictionary<string, int>());
+            bool success = await RedirectEntity.put(redirectTable, claimsPrincipal.Identity.Name, entity.RowKey, entity.RedirectTo, 0, new Dictionary<string, int>());
             if (!success) {
-                return new BadRequestObjectResult($"Error occurred creating ${data.rowKey} already exists for ${claimsPrincipal.Identity.Name}");
+                return new BadRequestObjectResult($"Error occurred creating {entity.RowKey} already exists for {claimsPrincipal.Identity.Name}");
             }
 
             return new OkResult();
@@ -109,21 +109,21 @@ namespace api.v1
             }
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            RedirectEntity entity = JsonConvert.DeserializeObject<RedirectEntity>(requestBody);
 
-            if (data?.rowKey == null || data?.redirectTo == null) {
+            if (entity.RowKey == null || entity.RedirectTo == null) {
                 return new BadRequestObjectResult($"Please specify the key and redirectTo parameters in the request body");
             }
 
-            log.LogInformation($"Getting Redirect row for values ${claimsPrincipal.Identity.Name} and ${data.rowKey}");
-            RedirectEntity existingEntity = await RedirectEntity.get(redirectTable, claimsPrincipal.Identity.Name, data.rowKey);
+            log.LogInformation($"Getting Redirect row for values {claimsPrincipal.Identity.Name} and {entity.RowKey}");
+            RedirectEntity existingEntity = await RedirectEntity.get(redirectTable, claimsPrincipal.Identity.Name, entity.RowKey);
             if (existingEntity == null) {
-                return new BadRequestObjectResult($"Redirect with ${data.rowKey} doesn't exist for ${claimsPrincipal.Identity.Name}");
+                return new BadRequestObjectResult($"Redirect with {entity.RowKey} doesn't exist for {claimsPrincipal.Identity.Name}");
             }
 
-            bool success = await RedirectEntity.put(redirectTable, claimsPrincipal.Identity.Name, data.rowKey, data.redirectTo, 0, new Dictionary<string, int>());
+            bool success = await RedirectEntity.put(redirectTable, claimsPrincipal.Identity.Name, entity.RowKey, entity.RedirectTo, 0, new Dictionary<string, int>());
             if (!success) {
-                return new BadRequestObjectResult($"Error occurred updating ${data.rowKey} for ${claimsPrincipal.Identity.Name}");
+                return new BadRequestObjectResult($"Error occurred updating {entity.RowKey} for {claimsPrincipal.Identity.Name}");
             }
 
             return new OkResult();
