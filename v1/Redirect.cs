495adc94b96ce0ae18d55537af6c3d67587473fb
Mark Powney <mpowney+github@icloud.com>
2020-05-11 23:28:06 +1000

    update route order

--- v1/Redirect.cs
+++ v1/Redirect.cs
@@ -123,6 +123,30 @@ namespace api.v1
 
         }
 
+        [FunctionName("RedirectGetGeoCounts")]
+        public static async Task<IActionResult> RedirectGetGeoCounts (
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "_api/v1/redirect/{key:regex([\\w\\d]+)}/geo")] HttpRequest req,
+            [Table(TableNames.Redirects)] CloudTable redirectTable,
+            [Table(TableNames.Geos)] CloudTable geoTable,
+            string key,
+            ILogger log,
+            ExecutionContext context,
+            ClaimsPrincipal claimsPrincipal)
+        {
+
+            if (!claimsPrincipal.Identity.IsAuthenticated) {
+                return new UnauthorizedResult();
+            }
+
+            RedirectEntity entity = await RedirectEntity.get(redirectTable, claimsPrincipal.Identity.Name, key);
+            if (entity == null) {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(await entity.GetGeoCount(geoTable));
+
+        }
+
         [FunctionName("RedirectAndHostGet")]
         public static async Task<IActionResult> RedirectAndHostGet (
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "_api/v1/redirect/{host}/{key}")] HttpRequest req,
@@ -150,30 +174,6 @@ namespace api.v1
             }
 
             return new OkObjectResult(new RedirectEntity(entity.PartitionKey, entity.RowKey, entity.RedirectTo, 0, new Dictionary<string, int>(), DateTime.Now, false));
-
-        }
-
-        [FunctionName("RedirectGetGeoCounts")]
-        public static async Task<IActionResult> RedirectGetGeoCounts (
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "_api/v1/redirect/{key:regex([\\w\\d]+)}/geo")] HttpRequest req,
-            [Table(TableNames.Redirects)] CloudTable redirectTable,
-            [Table(TableNames.Geos)] CloudTable geoTable,
-            string key,
-            ILogger log,
-            ExecutionContext context,
-            ClaimsPrincipal claimsPrincipal)
-        {
-
-            if (!claimsPrincipal.Identity.IsAuthenticated) {
-                return new UnauthorizedResult();
-            }
-
-            RedirectEntity entity = await RedirectEntity.get(redirectTable, claimsPrincipal.Identity.Name, key);
-            if (entity == null) {
-                return new NotFoundResult();
-            }
-
-            return new OkObjectResult(await entity.GetGeoCount(geoTable));
 
         }
 
